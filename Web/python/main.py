faf520ed57bae1971fd2fbf8268ce3d36c5394b0
Fan Gaoyige <52692024+caesarvan@users.noreply.github.com>
2022-03-30 06:02:23 +0800

    split tribo and piezoresis for machine learning

--- Web/python/main.py
+++ Web/python/main.py
@@ -3,7 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 fb_app = firebase.FirebaseApplication('https://arduinoble-ios-default-rtdb.asia-southeast1.firebasedatabase.app', None)
-result = fb_app.get('/patient1/data/2022-03-26', None)
+result = fb_app.get('/patient1/data/2022-03-27', None)
 
 list = []
 # get key-value pairs from results
@@ -11,10 +11,11 @@ for key, value in result.items():
     list.append(value)
 
 list = np.array(list)
+list = list[31000:31200,:]
 print(list.shape)
 
 # plot the data
 for j in range(list.shape[1]):
     plt.plot(list[:, j], label=str(j))
-plt.legend()
+# plt.legend()
 plt.show()
